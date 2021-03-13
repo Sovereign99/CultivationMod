7bade30ab67966039dcbda9f5158e94a72a71ac6
Karsten Hald <71352486+KarstenH99@users.noreply.github.com>
2021-03-13 00:29:24 -0800

    Updated recipes
    Made the recipes actually work

--- src/main/java/net/sovereign/cultivation/data/ModRecipeProvider.java
+++ src/main/java/net/sovereign/cultivation/data/ModRecipeProvider.java
@@ -3,6 +3,8 @@ package net.sovereign.cultivation.data;
 import net.minecraft.data.*;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+import net.sovereign.cultivation.CultivationMod;
 import net.sovereign.cultivation.setup.ModBlocks;
 import net.sovereign.cultivation.setup.ModItems;
 
@@ -19,23 +21,36 @@ public class ModRecipeProvider extends RecipeProvider {
         ShapelessRecipeBuilder.shapelessRecipe(ModItems.CELESTIAL_SILVER_INGOT.get(), 9)
                 .addIngredient(ModBlocks.CELESTIAL_SILVER_BLOCK.get()).addCriterion("has_item",
                 hasItem(ModItems.CELESTIAL_SILVER_INGOT.get())).build(consumer);
+
         //Ore to ingot, for furnace and blast furnace
-        CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(ModBlocks.CELESTIAL_SILVER_ORE.get()), ModItems.CELESTIAL_SILVER_INGOT.get(), 50.0f, 800);
-        CookingRecipeBuilder.blastingRecipe(Ingredient.fromItems(ModBlocks.CELESTIAL_SILVER_ORE.get()), ModItems.CELESTIAL_SILVER_INGOT.get(), 50.0f, 400);
+        CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(ModBlocks.CELESTIAL_SILVER_ORE.get()), ModItems.CELESTIAL_SILVER_INGOT.get(), 50.0f, 800)
+                .addCriterion("has_item", hasItem(ModBlocks.CELESTIAL_SILVER_ORE.get()))
+                .build(consumer, modId("silver_ingot_smelting"));
+        CookingRecipeBuilder.blastingRecipe(Ingredient.fromItems(ModBlocks.CELESTIAL_SILVER_ORE.get()), ModItems.CELESTIAL_SILVER_INGOT.get(), 50.0f, 400)
+                .addCriterion("has_item", hasItem(ModBlocks.CELESTIAL_SILVER_ORE.get()))
+                .build(consumer, modId("silver_ingot_blasting"));
 
         //Ingot to storage block
-        ShapedRecipeBuilder.shapedRecipe(ModBlocks.CELESTIAL_SILVER_BLOCK.get()).key('#', ModItems.CELESTIAL_SILVER_INGOT.get())
+        ShapedRecipeBuilder.shapedRecipe(ModBlocks.CELESTIAL_SILVER_BLOCK.get())
+                .key('#', ModItems.CELESTIAL_SILVER_INGOT.get())
                 .patternLine("###")
                 .patternLine("###")
-                .patternLine("###").addCriterion("has_item", hasItem(ModItems.CELESTIAL_SILVER_INGOT.get()))
+                .patternLine("###")
+                .addCriterion("has_item", hasItem(ModItems.CELESTIAL_SILVER_INGOT.get()))
                 .build(consumer);
 
         //Sword recipe
-        ShapedRecipeBuilder.shapedRecipe(ModItems.CELESTIAL_SILVER_SWORD.get()).key('#', ModItems.CELESTIAL_SILVER_INGOT.get())
-                .key('/', Items.STICK.getItem())
+        ShapedRecipeBuilder.shapedRecipe(ModItems.CELESTIAL_SILVER_SWORD.get())
+                .key('#', ModItems.CELESTIAL_SILVER_INGOT.get())
+                .key('/', Items.STICK)
                 .patternLine(" # ")
                 .patternLine(" # ")
                 .patternLine(" / ")
-                .build(consumer);
+                .addCriterion("has_item", hasItem(ModItems.CELESTIAL_SILVER_INGOT.get()))
+                .build(consumer, modId("silver_sword"));
+    }
+
+    private static ResourceLocation modId(String path) {
+        return new ResourceLocation(CultivationMod.MOD_ID, path);
     }
 }
