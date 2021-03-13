301ceb3a97a6221d533443d5f2e9b8ed88b89849
Karsten Hald <71352486+KarstenH99@users.noreply.github.com>
2021-03-13 01:23:33 -0800

    Update ModBlocks.java
    Made it look nicer, set blocks to require tools

--- src/main/java/net/sovereign/cultivation/setup/ModBlocks.java
+++ src/main/java/net/sovereign/cultivation/setup/ModBlocks.java
@@ -14,9 +14,18 @@ import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final RegistryObject<Block> CELESTIAL_SILVER_ORE = register("celestial_silver_ore", () ->
-            new Block(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(50, 10).harvestLevel(3).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE)));
+            new Block(AbstractBlock.Properties.create(Material.ROCK)
+                    .hardnessAndResistance(50, 10)
+                    .harvestLevel(3)
+                    .sound(SoundType.STONE)
+                    .setRequiresTool()
+                    .harvestTool(ToolType.PICKAXE)));
     public static final RegistryObject<Block> CELESTIAL_SILVER_BLOCK = register("celestial_silver_block", () ->
-            new Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(3, 10).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE)));
+            new Block(AbstractBlock.Properties.create(Material.IRON)
+                    .hardnessAndResistance(3, 10)
+                    .sound(SoundType.METAL)
+                    .setRequiresTool()
+                    .harvestTool(ToolType.PICKAXE)));
 
     static void register() {}
 
