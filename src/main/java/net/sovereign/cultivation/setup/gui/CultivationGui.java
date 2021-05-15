a311075fe36ea2c7df21dad86cee2dafba266e69
Sovereign99 <84218111+Sovereign99@users.noreply.github.com>
2021-05-14 22:16:46 -0700

    Update CultivationGui.java

--- src/main/java/net/sovereign/cultivation/setup/gui/CultivationGui.java
+++ src/main/java/net/sovereign/cultivation/setup/gui/CultivationGui.java
@@ -15,13 +15,13 @@ public class CultivationGui extends GuiUtils {
     private static final int WIDTH = 102, HEIGHT = 8, BAR_WIDTH = 100, BAR_HEIGHT = 6;
 
     public static void cultivationOverlay(RenderGameOverlayEvent.Post event) {
-        if(event.getType() == RenderGameOverlayEvent.ElementType.EXPERIENCE) {
+        if(event.getType() == RenderGameOverlayEvent.ElementType.TEXT) {
             Minecraft mc = Minecraft.getInstance();
             mc.textureManager.bindTexture(BAR);
             
             ICultivation cultivation = mc.player.getCapability(Cultivation.CULTIVATION_CAP).orElse(new Cultivation());
-            float oneUnit = (float) (BAR_WIDTH / 1000);
-            int currentWidth = (int) (oneUnit * cultivation.getCultivationAmount());
+            float oneUnit = (float) (1000 / BAR_WIDTH);
+            int currentWidth = (int) (oneUnit * (cultivation.getCultivationAmount() % 1000));
             drawTexturedModalRect(event.getMatrixStack(), 0, 0, 0, 0, WIDTH, HEIGHT, 0);
             drawTexturedModalRect(event.getMatrixStack(), 1, 0, 1, HEIGHT, currentWidth, HEIGHT, 1);
         }
